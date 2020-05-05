60c37d415051ced600a3f0f622139afaa8e8fbfd
Oleg Loutchansky <luchansodev@gmail.com>
2020-05-05 21:11:36 +0300

    feat: some format

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 mod mem_lib;
 
 fn main() {
-    let pid = mem_lib::get_proc_id_by_name("dota2.exe");
-    println!("Found process with id {}", pid);
-    // mem_lib::get_proc_id_by_name("dota2.exe");
+    let process_name = "dota2.exe";
+    let pid = mem_lib::get_proc_id_by_name(process_name);
+    println!("Found {} with id {}", process_name, pid);
 
     let process = mem_lib::get_proc(pid);
 
--- src/mem_lib.rs
+++ src/mem_lib.rs
@@ -1,16 +1,16 @@
 extern crate winapi;
 extern crate wio;
 
-use winapi::shared::ntdef::HANDLE;
-use winapi::ctypes::{c_void};
-use winapi::um::winnt::{PROCESS_VM_READ};
-use winapi::um::{tlhelp32};
-use std::mem;
-use std::ffi::OsString;
-use std::os::windows::prelude::*;
-use winapi::um::memoryapi::{ReadProcessMemory};
-use winapi::um::processthreadsapi::{OpenProcess};
 use std::convert::TryInto;
+use std::ffi::OsString;
+use std::mem;
+use std::os::windows::prelude::*;
+use winapi::ctypes::c_void;
+use winapi::shared::ntdef::HANDLE;
+use winapi::um::memoryapi::ReadProcessMemory;
+use winapi::um::processthreadsapi::OpenProcess;
+use winapi::um::tlhelp32;
+use winapi::um::winnt::PROCESS_VM_READ;
 
 pub struct Process {
     handler: HANDLE,
@@ -39,33 +39,42 @@ impl Process {
 }
 
 pub fn get_proc(pid: u32) -> Process {
-    return Process::new(unsafe{OpenProcess(PROCESS_VM_READ, 0, pid)});
+    return Process::new(unsafe { OpenProcess(PROCESS_VM_READ, 0, pid) });
 }
 
 pub fn get_proc_id_by_name(name: &str) -> u32 {
+    let mut process: tlhelp32::PROCESSENTRY32W = unsafe { std::mem::zeroed() };
 
-    let mut process: tlhelp32::PROCESSENTRY32W = unsafe{ std::mem::zeroed() };
+    // Если не инициализировать dwSize, первый вызов Process32FirstW() упадёт с ошибкой
+    // https://docs.microsoft.com/en-us/windows/win32/api/tlhelp32/ns-tlhelp32-processentry32#members
     process.dwSize = mem::size_of::<tlhelp32::PROCESSENTRY32W>() as u32;
 
-    //Make a Snanshot of all the current proccess.
-    let snapshot = unsafe{tlhelp32::CreateToolhelp32Snapshot(tlhelp32::TH32CS_SNAPPROCESS, 0)};
+    // Создаём снепшот всех процессов
+    // https://docs.microsoft.com/en-us/windows/win32/api/tlhelp32/nf-tlhelp32-createtoolhelp32snapshot
+    let snapshot = unsafe { tlhelp32::CreateToolhelp32Snapshot(tlhelp32::TH32CS_SNAPPROCESS, 0) };
 
-    //Get the first proccess and store it in proccess variable.
-    if unsafe{tlhelp32::Process32FirstW(snapshot, &mut process)} != 0{
-
-        //Take the next procces if posible.
-        while unsafe{tlhelp32::Process32NextW(snapshot, &mut process)} != 0 {
-
+    // Получаем данные о первом процессе из снепшота, только так мы можем потом вызывать Process32NextW
+    // https://docs.microsoft.com/en-us/windows/win32/api/tlhelp32/nf-tlhelp32-process32firstw
+    if unsafe { tlhelp32::Process32FirstW(snapshot, &mut process) } != 0 {
+        // Получаем данные о следующем процессе в снепшоте, до тех пор пока функция не вернула 0
+        // https://docs.microsoft.com/en-us/windows/win32/api/tlhelp32/nf-tlhelp32-process32nextw
+        while unsafe { tlhelp32::Process32NextW(snapshot, &mut process) } != 0 {
             let process_name = OsString::from_wide(&process.szExeFile);
+
+            // process_name
+            println!("check process: {}", process_name.to_string_lossy());
 
             match process_name.into_string() {
                 Ok(s) => {
                     if s.contains(name) {
                         return process.th32ProcessID;
                     }
-                },
+                }
                 Err(_) => {
-                    println!("Error converting process name for PID {}", process.th32ProcessID);
+                    println!(
+                        "Error converting process name for PID {}",
+                        process.th32ProcessID
+                    );
                 }
             }
         }
